b18fad0e25a83d214915c2837d8088c1e3d9c465
antonBlagodarnyy <antonbgi98@gmail.com>
2025-03-23 12:55:47 +0100

    Created backend for login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -14,7 +14,38 @@ exports.createUser = (req, res) => {
       "insert into USER (USERNAME, EMAIL, PWD_HASH, AUTH_TYPE, EXTERNAL_ID) values (?,?,?,?,?)";
     con.query(sql, [username, email, hash, authType, externalId], (err) => {
       if (err) return res.status(520).json({ error: err.name });
-      else  return res.status(201).json({message: "passed"});
+      else return res.status(201).json({ message: "passed" });
     });
+  });
+};
+
+exports.userLogin = (req, res) => {
+  const email = req.body.email;
+
+  var sql = "SELECT * FROM user WHERE email = ?";
+  con.query(sql, [email], (error, result) => {
+    if (result.length == 0) {
+      return res
+        .status(401)
+        .json({ message: "No user with that email was found" });
+    } else {
+      const user = result[0];
+      return bcrypt
+        .compare(req.body.password, result[0].password)
+        .then((result) => {
+          if (!result) {
+            return res.status(401).json({ message: "Password incorrect" });
+          } else {
+            const token = jwt.sign(
+              { email: user.email, userId: user.id },
+              process.env.JWT_KEY,
+              { expiresIn: "1h" }
+            );
+            res
+              .status(200)
+              .json({ token: token, expiresIn: 3600, userId: user.id });
+          }
+        });
+    }
   });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const UserController = require("../controllers/user");
 
 router.post("/signup", UserController.createUser);
+router.post("/login", UserController.userLogin);
 
 module.exports = router;

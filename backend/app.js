7c476e4d94792054be5310adcad3412139338b57
antonBlagodarnyy <antonbgi98@gmail.com>
2025-05-23 11:31:48 +0200

    fixed typo

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.user(
+app.use(
   cors({
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
